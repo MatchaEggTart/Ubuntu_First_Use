867767f949fd20a0af2c78c05a5c6e123ae6339b
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-01-12 00:30:32 +0800

    update

--- 02_install_tools/01_install_edge.sh
+++ 02_install_tools/01_install_edge.sh
@@ -2,4 +2,5 @@ sudo apt update
 sudo apt install software-properties-common apt-transport-https wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main"
-sudo apt install microsoft-edge-dev
+# sudo apt install microsoft-edge-dev
+sudo apt install microsoft-edge-stable -y
