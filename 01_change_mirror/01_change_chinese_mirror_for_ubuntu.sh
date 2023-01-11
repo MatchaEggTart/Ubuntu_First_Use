7772dafea8c969b7f86b52cb68f30f4e34d74d0d
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-01-12 00:33:40 +0800

    update

--- 01_change_mirror/01_change_chinese_mirror_for_ubuntu.sh
+++ 01_change_mirror/01_change_chinese_mirror_for_ubuntu.sh
@@ -1,3 +1,5 @@
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
 sudo sed -i "s@http://.*archive.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
 sudo sed -i "s@http://.*security.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
+sudo apt update
+sudo apt upgrade -y
