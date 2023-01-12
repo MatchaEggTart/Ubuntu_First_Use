0d703be063b9eb5b6caf6201306ae11571413d3b
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-01-12 12:34:30 +0800

    update

--- 02_install_tools/10_install_tweaks.sh
+++ 02_install_tools/10_install_tweaks.sh
@@ -1,6 +1,7 @@
 sudo add-apt-repository universe -y
 sudo apt install gnome-tweaks -y
-sudo apt install gnome-shell gnome-shell-extensions -y
+sudo apt install gnome-shell gnome-shell-extensions chrome-gnome-shell -y
+sudo apt install gnome-shell-extension-manager -y
 
 # apt search gnome-shell-extension
 # sudo apt install  gnome-shell-extension-gsconnect
